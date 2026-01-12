5774fd0ba7ab2573ea32bfe029abf0424b8f17f7
unknown <maazjaipuri2@gmail.com>
2026-01-12 14:51:14 +0530

    update

---
+++ Marksheet/script.js
@@ -0,0 +1,14 @@
+let Name = prompt("Enter student name");
+let rollno = prompt("Enter rollno :");
+let English_marks = prompt("Enter english_marks");
+let Mathematics_marks = prompt("Enter Mathematics_marks");
+let Computer_marks = prompt("Enter Computer_marks");
+
+
+let total = English + Mathematics + Computer;
+
+document.getElementById('Name').innerText = Name;
+document.getElementById('rollno').innerText = rollno;
+document.getElementById('English').innerText =English_marks;
+document.getElementById('mathematics').innerText =Mathematics_marks;
+document.getElementById('computer').innerText =Computer_marks;
